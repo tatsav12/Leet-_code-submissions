d632138515e5aff1cb5cf015b8e906c490c7bc0c
tatsav12 <tatsavgangani1207@gmail.com>
2026-09-18 13:21:35 +0000

    Sync LeetCode submission Runtime - 122 ms (37.09%), Memory - 142.2 MB (25.19%)

--- my-workspace/0219-contains-duplicate-ii/solution.cpp
+++ my-workspace/0219-contains-duplicate-ii/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-
-        for(int i =0;i<nums.size();i++){
-            if(mp.find(nums[i])!=mp.end() && abs(mp[nums[i]]-i)<=k){
-                return true;
-            }
-            mp[nums[i]] = i;
+        unordered_map<int,int> m;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            if(m.find(nums[i])==m.end()){
+                m[nums[i]] = i;
+            }else if(i - m[nums[i]] <=k)return true;
+            else m[nums[i]] = i;
         }
         return false;
     }
