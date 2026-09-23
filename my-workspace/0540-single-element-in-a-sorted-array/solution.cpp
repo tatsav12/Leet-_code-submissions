00b8d3d58ad0c1a13cf58b71c1c115ec38104bed
tatsav12 <tatsavgangani1207@gmail.com>
2026-09-23 13:31:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.1 MB (94.98%)

--- my-workspace/0540-single-element-in-a-sorted-array/solution.cpp
+++ my-workspace/0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-    int st = 0,end = nums.size()-1;
-
-    
-   
-    while(st<=end){
-        int mid = st +(end-st)/2;
-        
-        if(mid == 0 || mid == nums.size()-1){
-            return nums[mid];
-        }
-
-        if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-            return nums[mid];
-        }
-        if(mid % 2 == 0){
-        if(nums[mid]==nums[mid-1]){
-            end = mid-1;
-        }else{
-            st = mid+1;
-        }}else{
-            if(nums[mid]==nums[mid-1]){
-                st = mid + 1;
+        int st = 0;
+        int end = nums.size()-1;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(mid==0 || mid == nums.size() ||
+            nums[mid]!=nums[mid+1]&&nums[mid]!=nums[mid-1])return nums[mid];
+            if(mid%2 == 0){
+                if(nums[mid]==nums[mid-1])end = mid-1;
+                else st = mid+1;
             }else{
-                end = mid -1;
+                if(nums[mid]==nums[mid+1])end = mid-1;
+                else st = mid+1;
             }
         }
-    }
-    return 0;
-    
+        return -1;
     }
 };
